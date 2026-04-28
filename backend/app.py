b6d3832a574eb7a271f373af9dc9e33c0c4c921e
Sneha-T8015 <snehathangaraj8015@gmail.com>
2026-04-28 12:54:23 +0530

    Main

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,11 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from db import db
 from models.tour import Tour
+import os
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 app = Flask(__name__)
 CORS(app)
 
