a18a1f7464bafb3be12b7bcebd3a12d49e11ac9e
Sneha-T8015 <snehathangaraj8015@gmail.com>
2026-04-28 13:09:30 +0530

    Main

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,10 @@ import os
 
 app = Flask(__name__) 
 
+@app.route("/")
+def home():
+    return "Travel Tour Backend is running!"
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
