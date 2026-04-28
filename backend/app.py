9a1664acd1d2500b230e7b27685218d999495c22
Sneha-T8015 <snehathangaraj8015@gmail.com>
2026-04-28 13:06:54 +0530

    Main

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,8 @@ from db import db
 from models.tour import Tour
 import os
 
+app = Flask(__name__) 
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
